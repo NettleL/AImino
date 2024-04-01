a6247001227a05300b5ec0f3a93cdaf766e9fa47
NettleL <nethya.liyanage@education.nsw.gov.au>
2024-04-02 06:12:46 +1100

    Added a few comments for clarification

--- Assessment-Task-1/Assessment-Task-1.py
+++ Assessment-Task-1/Assessment-Task-1.py
@@ -1,12 +1,15 @@
 import time, os
 import random
 from colorama import Fore, Back, Style
-from SubjectData import *
-from Colour import *
-#NOTE - WHAT ABOUT QUIZZZLET
+from SubjectData import * #Contains all subject data - e.g questions/answers + content
+from Colour import * #Contains colour function (to colour text)
+
+#Initiates global variable for while loops
 subjectchoice = ''
 activitychoice = ''
 
+# STAT LISTS
+# Will ontain user stats for respective subject quizzes - list of tuplets (time, score of each attempt)
 physicsstats = []
 biostats = []
 chemstats = []
@@ -29,21 +32,21 @@ ___  ___  __  ___       ___ ___
                                                               
       ''')
     
-def subjectchoicefunction():
+def subjectchoicefunction(): #Checks if subject choice is valid
     global subjectchoice
     subjectchoice = input(colr('SUBJECT CHOICE (1, 2, 3, 4 or exit): '))
-    while subjectchoice not in ['1', '2', '3', '4', 'exit']:
+    while subjectchoice not in ['1', '2', '3', '4', 'exit']: #While subject choice is not valid
         print('Please enter a valid response - 1, 2, 3, 4 or exit')
-        subjectchoice = input(colr('SUBJECT CHOICE (1, 2, 3, 4 or exit): '))
-    return subjectchoice
+        subjectchoice = input(colr('SUBJECT CHOICE (1, 2, 3, 4 or exit): ')) #redo subject choice
+    return subjectchoice #returns valid subject choive
 
-def activitychoicefunction():
+def activitychoicefunction(): #Checks if activity choice (content/quiz) is valid
     global activitychoice
     activitychoice = input(colr('SECTION CHOICE (1, 2 or exit): '))
-    while activitychoice not in ['1', '2', 'exit']:
+    while activitychoice not in ['1', '2', 'exit']: #While not valid
         print('Please enter a valid response - 1, 2 or exit')
-        activitychoice = input(colr('SECTION CHOICE (1, 2 or exit): '))
-    return activitychoice
+        activitychoice = input(colr('SECTION CHOICE (1, 2 or exit): ')) #redo activity choice
+    return activitychoice #returns valid activity choice
 
 def bar(score,name): #defines the function that displays the bar
     newbar = (f'''{colr(name)}
@@ -58,7 +61,7 @@ ______{'___'*score}
     # for each new score (bar becomes bigger/smaller based on score)
     return newbar
 
-def answer(): #gets answer, checks validity of answer and returns answer when valid.
+def answer(): #gets answer, checks validity of answer and returns answer when valid - similar to subject + activity choice functions
   ans = input('→ ')
   while ans not in ['a','b','c','d']:
     print('Invalid. Please enter in either a, b, c or d')
@@ -69,7 +72,7 @@ def questions(dict, score): #asks questions from dictionary, checks answer and a
     correct = ''
     tempqs = dict.copy() # creates a temporary dictionary copy (so questions can be deleted from tempdict after they are asked, but not from the actual dictionary - therefore it can be accessed when the quiz is reattempted.)
     while tempqs != {}: #while the temporary dictionary has not run out of questions
-        print(bar(score, name))
+        print(bar(score, name)) #show the score on the progress bar
         qno = random.choice(list(tempqs.keys())) #picks a random key from dictionary
         print(tempqs[qno][0]) #prints the question
         print('')
@@ -77,35 +80,35 @@ def questions(dict, score): #asks questions from dictionary, checks answer and a
         if ans == tempqs[qno][1]: #if answer is correct
             correct = colr('Correct')
             score = score + 1
-        else:
-            correct = (f'''{colr('Wrong')}
+        else: #If answer is wrong, show wrong and display explanation
+            correct = (f'''{colr('Wrong')} 
 {tempqs[qno][2]}''')
         del tempqs[qno] #deletes q. from tempdict so its not asked again in the quiz attempt
         cleartitle()
-        print(correct)
+        print(correct) #prints either 'correct' or 'wrong + explanation
         print('')
-    return score 
+    return score #returns total score of that quiz (i.e three questions)
 
-def table(stats):
+def table(stats): #Creates a table based on stats
     print('''
 +---------+----------+---------+
 | ATTEMPT | TIME (s) |  SCORE  |
-+---------+----------+---------+''')
++---------+----------+---------+''') 
     attempt = 1 #IS AN INTEGER
-    for stat in stats:
-        if len(str(stat[0])) > 6:
+    for stat in stats: #For every row needed
+        if len(str(stat[0])) > 6: #If too many digits to align nicely
             alignedtime = 'ERROR!'
         else:
-            alignedtime = str(stat[0]).ljust(6)
+            alignedtime = str(stat[0]).ljust(6) #Aligns nicely - padding
         if attempt > 999:
-            alignedattempt = attempt
+            alignedattempt = attempt #At this point honestly i don't think they'll care if the tables messed up
         else:
-            alignedattempt = str(attempt).ljust(3)
+            alignedattempt = str(attempt).ljust(3) #Aligns nicely - padding
         print(f'''|  {colr(alignedattempt)}    |  {colr(alignedtime)}  |    {colr(str(stat[1]))}    |
-+---------+----------+---------+''')
++---------+----------+---------+''') #creates a new row
         attempt += 1
 
-def alltables(subject,dictionary):
+def alltables(subject,dictionary): #Just some formatting for Option 4. of main menu - scoreboards
     print(colr('─────'), bold(subject), colr('─────'))
     table(dictionary)
     if dictionary == []:
@@ -113,12 +116,14 @@ def alltables(subject,dictionary):
 +---------+----------+---------+''')
     print('')
     
-def subjectfunction(subject):
-    content = ''
-    qeasy = {}
-    qmid = {}
-    qhard = {}
-    if subject == 'physics':
+def subjectfunction(subject): #Main function for everything in a subject.
+    content = '' #content of the subject selected
+    qeasy = {}  #easy questions of the subject selected
+    qmid = {}   #medium questions of the usbject selected
+    qhard = {}  #hard questions of the subject selected
+    
+    #Establishes the content and questions for each subject
+    if subject == 'physics': 
         content = physicscontent
         qeasy = physicsqseasy
         qmid = physicsqsmid
@@ -136,45 +141,46 @@ def subjectfunction(subject):
         qmid = chemqsmid
         qhard = chemqshard
         finstats = chemstats
+    
     cleartitle()
-    print(activityinfo)
-    activitychoicefunction()
-    while activitychoice != 'exit':
-        while activitychoice == '1':
+    print(activityinfo) #activity menu
+    activitychoicefunction() #asks user for activity choice and returns valid choice.
+    while activitychoice != 'exit': 
+        while activitychoice == '1': #content
             cleartitle()
-            print(content)
-            input(colr('Please enter in anything to continue: '))
+            print(content) #prints content
+            input(colr('Please enter in anything to continue: ')) #User can continue once they finished reading
             cleartitle()
-            print(activityinfo)
+            print(activityinfo) #returns to activity menu
             activitychoicefunction()
-        while activitychoice == '2':
+        while activitychoice == '2': #quiz
             cleartitle()
             print(questioninfo)
-            input(colr('Please enter in anything to start the quiz: '))
+            input(colr('Please enter in anything to start the quiz: ')) #user can start when they want
             cleartitle()
-            ### ADD QUESTIONS INFORMATION
-            starttime = time.time()
-            sceasy = questions(qeasy, 0)
-            scmid = questions(qmid, sceasy)
-            schard = questions(qhard, scmid)
-            endtime = time.time()
+            starttime = time.time() #timer starts
+            sceasy = questions(qeasy, 0) #easy qs + gets score
+            scmid = questions(qmid, sceasy) #medium qs + adds score to easyq score
+            schard = questions(qhard, scmid) #hard qs + adds score to the easyq and midq score
+            endtime = time.time() #timer stops
             total_time = endtime - starttime #IS FLOAT
-            finstats.append(tuple([round(total_time,2),schard]))
+            finstats.append(tuple([round(total_time,2),schard])) #appends the time and total score to the subject stats list
+            #shows final score (on bar and directly)
             print(bar(schard,name))
             print(colr('FINAL SCORE ='), schard)
+            #makes a pretty table
             table(finstats)
             print('')
             print('NOTE: If Time (s) displays (ERROR!), the attempt time was > 999999 seconds')
-            #make a pretty table
-            input(colr('Please enter in anything to continue: '))
+            input(colr('Please enter in anything to continue: ')) #user can continue whenever they want
             cleartitle()
-            print(activityinfo)
+            print(activityinfo) #activity menu
             activitychoicefunction()
     cleartitle()
     print(programinfo)
     subjectchoicefunction()
 
-#_________________________________________________________________________________
+#_________________________________________________________________________________ MAIN
 clearscreen()
 #INITIAL TITLE
 print(f'''
@@ -194,7 +200,7 @@ print(f'''
 All content is taken from Zhang, J. et al., Oxford Insight Science 8 student book, Victoria, Oxford University Press, 2014.
 
 ''')
-input(colr('Please enter in anything to start: '))
+input(colr('Please enter in anything to start: ')) #User can continue at their own pace after reading disclaimer
 cleartitle()
 
 name = input(colr('PLEASE ENTER YOUR NAME: ')) #gets name
@@ -230,11 +236,11 @@ This is timed {colr(':)')}
 ''')
 
 print(programinfo)
-subjectchoicefunction()
+subjectchoicefunction() #first subject choice
 
-while subjectchoice != 'exit':
+while subjectchoice != 'exit': #while the subject choice is not exit
     #PHYSICS
-    while subjectchoice == '1':
+    while subjectchoice == '1': 
         subjectfunction('physics')
         
     # BIOLOGY
@@ -248,22 +254,24 @@ while subjectchoice != 'exit':
     #SCOREBOARD
     while subjectchoice == '4':
         cleartitle() 
-        if physicsstats == [] and biostats == [] and chemstats == []:
+        if physicsstats == [] and biostats == [] and chemstats == []: #If the user has not attempted any quizzes
             print(f'{colr("DO A QUIZ!!! YOU'VE GOT THIS,")} {name.upper()}{colr('!')}')
         else:
             print(f'{colr("GOOD JOB,")} {name.upper()}{colr('!')}')
         print('')
-        alltables(' PHYSICS ',physicsstats)
+        #makes pretty tables
+        alltables(' PHYSICS ',physicsstats) 
         alltables(' BIOLOGY ',biostats)
         alltables('CHEMISTRY',chemstats)
         print('')
-        input(colr('Please enter in anything to continue: '))
+        input(colr('Please enter in anything to continue: ')) #user can continue when they want
         cleartitle()
-        print(programinfo)
+        print(programinfo) #main menu
         subjectchoicefunction()
-        
+   
+#END     
 cleartitle()
-print(colr('FINAL SCORES :)'))
+print(colr('FINAL SCORES :)')) #final scores
 print('')
 alltables(' PHYSICS ',physicsstats)
 alltables(' BIOLOGY ',biostats)
@@ -271,6 +279,7 @@ alltables('CHEMISTRY',chemstats)
 print('')
 input(colr('Please enter in anything to continue: '))
 
+#END SCREEN
 clearscreen()
 print(f''' 
                                                                                                                    *****           *****
@@ -289,13 +298,4 @@ print(f'''
                                                                                                                              *
 {name.upper()}
 ''')
-
-
-
-
-
-
-
-
-
 
