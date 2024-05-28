80add985d621fb8753901e11932e309b8da1e509
NettleL <nethya.liyanage@education.nsw.gov.au>
2024-05-28 11:00:51 +1000

    Activity 4a-e

---
+++ Unit-2-Work/Activity4e.py
@@ -0,0 +1,14 @@
+class pet:
+    def __init__(self, name, species, age, vaxxed, ccard, address, owner='unknown', account=0):
+        self.name = name
+        self.species  = species
+        self.age = age
+        self.vaxxed = vaxxed
+        self.ccard = ccard
+        self.address = address
+        self.owner = owner
+        self.account = account
+
+Bonnie = pet("Bonnie", 'cat', "4", "True", '4321 3457 6232 3243', '419a Windsor Road, Baulkham Hills, 2153', "Connie Connel", 1000)
+Bonnie = pet("Foxy", 'dog', "4", "True", '4321 3457 6232 3243', '419a Windsor Road, Baulkham Hills, 2153', "Connie Connel", 1000)
+print(Bonnie.vaxxed)
