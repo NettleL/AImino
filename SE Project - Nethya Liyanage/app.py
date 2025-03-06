a5e3f1b0f72490a8ff427cf0383b6c0fe01b1baa
NettleL <nethya.liyanage@education.nsw.gov.au>
2025-03-07 10:52:51 +1100

    added functionality to upload file - once file is uploaded user is sent to home screen (neede to save file + process file)

--- SE Project - Nethya Liyanage/app.py
+++ SE Project - Nethya Liyanage/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
@@ -6,9 +8,33 @@ app = Flask(__name__)
 def home():
     return render_template('protein_viewer.html')
 
+upload_folder = 'SE Project - Nethya Liyanage/misc'
+allowed_extensions = {'pdb', 'cif'}
+app.config['upload_folder'] = upload_folder
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in allowed_extensions
+    
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
-    pass
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file attached in request')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename=='':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['upload_folder'], filename))
+            return redirect(url_for('model', name=filename))
+    return render_template('protein_viewer.html')
+
+@app.route('/model')
+def model():
+    return render_template('protein_viewer.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
