55bc88cec8f6ea36e3ad608422dc77eb5b96a9af
NettleL <nethya.liyanage@education.nsw.gov.au>
2025-03-07 09:20:10 +1100

    edited protein_viewer.html to account for best practices

--- SE Project - Nethya Liyanage/app.py
+++ SE Project - Nethya Liyanage/app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template('protein_viewer.html')
 
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    pass
+
 if __name__ == '__main__':
     app.run(debug=True)
     
