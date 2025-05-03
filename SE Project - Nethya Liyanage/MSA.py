9ff61be1bea591c9c54669da5cf7bac06128e360
NettleL <nethya.liyanage@education.nsw.gov.au>
2025-05-04 00:28:35 +1000

    MSA Homologous Sequences function!!!

---
+++ SE Project - Nethya Liyanage/MSA.py
@@ -0,0 +1,81 @@
+import requests
+import time
+import json
+
+def search(sequence):
+    url = "https://bioinformatics.lt/comer/search/api/submit"
+    
+    # Data
+    data = {
+    'sequence': sequence,
+    'database': 'PDB70',
+    'evalue': '0.001' 
+    # measures statistical significance of sequence alignement.
+    # lower evalue means sequence is more significant/similar
+    }
+    
+    # Request
+    try:
+        response = requests.post(url, data=data)
+        result = response.json()
+
+        if result.get('success'):
+            job_id = result.get('job_id')
+            print(f'Job Submitted. Job_id = {job_id}')
+            return job_id
+        else:
+            print('Error Submitting Job:', result.get('form_errors'))
+            return None
+    except requests.exceptions.RequestException as error_message:
+        print(f'Search Request failed: {error_message}')
+        return None
+
+def status(job_id):
+    url = f"https://bioinformatics.lt/comer/search/api/job_status/{job_id}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        status_data = response.json()
+        return status_data.get("status"), status_data.get('log')
+    else:
+        return None
+
+def results(job_id):
+    url = f"https://bioinformatics.lt/comer/search/api/results_json/{job_id}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
+
+def run(sequence):
+    job_id = search(sequence)
+    if job_id:
+        while True:
+            status_check, log_message = status(job_id)
+            if status_check == 'finished':
+                print('Complete. Fetching results')
+                
+                result_data = results(job_id)
+                
+                with open('homo_search_results.txt', 'w') as file:
+                    json.dump(result_data, file, indent=4) 
+                return(result_data)
+            
+            
+            elif status_check == 'failed':
+                print('Job failed.')
+                return None
+            else:
+                if log_message:
+                    print(log_message)
+                else:
+                    print('Waiting... ')
+                time.sleep(5)
+    else:
+        return None
+    
+sequence = '''>sp|P68871|HBB_HUMAN Hemoglobin subunit beta OS=Homo sapiens OX=9606 GN=HBB PE=1 SV=2
+MVHLTPEEKSAVTALWGKVNVDEVGGEALGRLLVVYPWTQRFFESFGDLSTPDAVMGNPK
+VKAHGKKVLGAFSDGLAHLDNLKGTFATLSELHCDKLHVDPENFRLLGNVLVCVLAHHFG
+KEFTPPVQAAYQKVVAGVANALAHKYH'''
+run(sequence)
--- SE Project - Nethya Liyanage/fasta_validation.py
+++ SE Project - Nethya Liyanage/fasta_validation.py
@@ -16,70 +16,74 @@ RETURN CODES
 3 there are characters in a sequence line other than [A-Za-z]
 '''
 
-file_path = 'SE Project - Nethya Liyanage/test/ligase0.fasta'
-seq_id = [] # list of ids of each sequence in fasta file
-seq_count = 0
-is_valid = True
-dictionary = {}
+def validation(file_path):
+    seq_id = [] # list of ids of each sequence in fasta file
+    seq_count = 0
+    is_valid = True
+    dictionary = {}
 
-with open(file_path) as file:
-    filelist = file.read().splitlines()
+    with open(file_path) as file:
+        filelist = file.read().splitlines()
 
-if file_path.endswith(".fasta"):
-    if filelist[0][0] == '>': # if the first character of the first line (i.e. the first character) is >
-        seq_count = 1 # no. of sequences in fasta file
-        line_count = 1 # no. of lines in fasta file
-        seq_id.append(filelist[0].split(maxsplit=1)[0]) # add identifier (from > up to first whitespace) to list
-        for line in filelist[1:]: # for each line after the first line (seq identifier no. 1)
-            line_count = line_count + 1
-            if line != '': # if line is not empty
-                if line[0] =='>': # if line begins with >, it is a sequence identifier
-                    seq_count = seq_count + 1
-                    if line.split(maxsplit=1)[0] in seq_id:
-                        is_valid = False
-                        dictionary[line_count] = 2
+    if file_path.endswith(".fasta"):
+        if filelist[0][0] == '>': # if the first character of the first line (i.e. the first character) is >
+            seq_count = 1 # no. of sequences in fasta file
+            line_count = 1 # no. of lines in fasta file
+            seq_id.append(filelist[0].split(maxsplit=1)[0]) # add identifier (from > up to first whitespace) to list
+            for line in filelist[1:]: # for each line after the first line (seq identifier no. 1)
+                line_count = line_count + 1
+                if line != '': # if line is not empty
+                    if line[0] =='>': # if line begins with >, it is a sequence identifier
+                        seq_count = seq_count + 1
+                        if line.split(maxsplit=1)[0] in seq_id:
+                            is_valid = False
+                            dictionary[line_count] = 2
+                        else:
+                            pass
+                        seq_id.append(line.split(maxsplit=1)[0])
+                    elif line.isalpha(): # if line is not sequence identifier, it must pass isalpha
+                        dictionary[line_count] = 0
                     else:
-                        pass
-                    seq_id.append(line.split(maxsplit=1)[0])
-                elif line.isalpha(): # if line is not sequence identifier, it must pass isalpha
-                    dictionary[line_count] = 0
+                        is_valid = False
+                        dictionary[line_count] = 3
                 else:
-                    is_valid = False
-                    dictionary[line_count] = 3
-            else:
-                pass # if line is empty, ignore
+                    pass # if line is empty, ignore
+        else:
+            is_valid = False
+            dictionary[1] = 1
+
     else:
         is_valid = False
-        dictionary[1] = 1
+        dictionary['Nil'] = 4
+        
+    # Validity
+    if is_valid:
+        print('VALID')
+    else:
+        print('INVALID')
 
-else:
-    is_valid = False
-    dictionary['Nil'] = 4
-    
-# Validity
-if is_valid:
-    print('VALID')
-else:
-    print('INVALID')
+    print('') 
+        
+    # Meta Info
+    print(f'Sequences: {seq_count}')
+    for s_id in seq_id:
+        print(s_id)
 
-print('') 
-    
-# Meta Info
-print(f'Sequences: {seq_count}')
-for s_id in seq_id:
-    print(s_id)
+    print('') 
+        
+    # Error Messages
+    for (key, value) in dictionary.items():
+        if value == 0:
+            pass
+        elif value == 1:
+            print(f'(Line {key}) Error 1: the first line does not start with a > (rest of file was not read & thus further issues can not be determined)')
+        elif value == 2:
+            print(f'(Line {key}) Error 2: there are duplicate sequence identifiers in the file')
+        elif value == 3:
+            print(f'(Line {key}) Error 3: there are characters in a sequence line other than [A-Za-z]')
+        elif value == 4:
+            print(f'(Line {key}) Error 4: Not a .FASTA file')
+    return(is_valid)
 
-print('') 
-    
-# Error Messages
-for (key, value) in dictionary.items():
-    if value == 0:
-        pass
-    elif value == 1:
-        print(f'(Line {key}) Error 1: the first line does not start with a > (rest of file was not read & thus further issues can not be determined)')
-    elif value == 2:
-        print(f'(Line {key}) Error 2: there are duplicate sequence identifiers in the file')
-    elif value == 3:
-        print(f'(Line {key}) Error 3: there are characters in a sequence line other than [A-Za-z]')
-    elif value == 4:
-        print(f'(Line {key}) Error 4: Not a .FASTA file')
+file = 'SE Project - Nethya Liyanage/test/ligase1.fasta'
+print(validation(file))
