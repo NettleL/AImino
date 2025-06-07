43d71b040a6537c3167a56e398eb0cabe30bcde0
NettleL <nethya.liyanage@education.nsw.gov.au>
2025-06-08 00:40:07 +1000

    misc additions

--- SE_Project_Nethya_Liyanage/app.py
+++ SE_Project_Nethya_Liyanage/app.py
@@ -1,6 +1,9 @@
 import os
 from flask import Flask, render_template, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
+import ast
+import torch
+from aimino.aimino import ProteinStructureModel # potentially unnecessary
 
 app = Flask(__name__)
 
@@ -8,6 +11,12 @@ upload_folder = r'C:\Users\nethy\OneDrive\Documents\Nethya\School\Year_12\12SE\6
 allowed_extensions = {'pdb', 'cif'}
 app.config['UPLOAD_FOLDER'] = upload_folder
 app.config['SECRET_KEY'] = 'manifestninetyninepointninefive'
+
+search_dict_path = r'C:\Users\nethy\OneDrive\Documents\Nethya\School\Year_12\12SE\6-Nethya-Liyanage\SE_Project_Nethya_Liyanage\training_data\test_fasta_dict.txt'
+with open(search_dict_path, 'r') as f:
+    search_dict = ast.literal_eval(f.read())
+test_dataset = torch.load(r'C:\Users\nethy\OneDrive\Documents\Nethya\School\Year_12\12SE\6-Nethya-Liyanage\SE_Project_Nethya_Liyanage\training_data\final_test_dataset.pt', map_location=torch.device('cpu'))
+test_dict = {sample[0]: sample for sample in test_dataset} # list comprehension :)
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -33,9 +42,11 @@ def upload():
             return redirect(url_for('model', name=filename))
     return render_template('upload.html')
 
-@app.route('/predict')
-def predict():
-    return render_template('predict.html')
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    # LIVE SEARCH BAR
+    return(render_template('search.html'))
+        
 
 @app.route('/model')
 def model():
