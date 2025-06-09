3e643a828e4af8426ceebd0fe4800720b0f2dec4
NettleL <nethya.liyanage@education.nsw.gov.au>
2025-06-09 17:10:02 +1000

    delete redundant/unused

--- sidechainnettest.py
+++
@@ -1,7 +0,0 @@
-import sidechainnet
-print("SidechainNet")
-
-'''
-python "c:/Users/nethy/OneDrive/Documents/Nethya/School/Year 12/12SE/6-Nethya-Liyanage/sidechainnettest.py"
-conda activate prody_env
-'''
