99bbdce78f1643eaa8e0208edcc141033b7d46f0
NettleL <nethya.liyanage@education.nsw.gov.au>
2025-06-10 16:51:06 +1000

    accidentally deleted app.py - restored

--- SE_Project_Nethya_Liyanage/aimino/download.py
+++ SE_Project_Nethya_Liyanage/aimino/download.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+from flask import current_app
 from aimino.plot_prot import classical_mds
 
 def create_pdb_file(dist_map):
@@ -11,7 +12,13 @@ def create_pdb_file(dist_map):
     return "\n".join(lines)
 
 def create_npz_file(dist_pred, dist_target, xyz_pred, xyz_target, key):
-    filename = f'{key}.npz'
-    file_path = os.path.join('static', 'uploads', filename)
-    np.savez(file_path, dist_pred=dist_pred, dist_target=dist_target, xyz_pred=xyz_pred, xyz_target=xyz_target)
+    filename = f'{key}_pred.npz'
+    download_folder = current_app.config['DOWNLOAD_FOLDER']
+    os.makedirs(download_folder, exist_ok=True)
+    file_path = os.path.join(download_folder, filename)
+    np.savez(file_path,
+             dist_pred=dist_pred,
+             dist_target=dist_target,
+             xyz_pred=xyz_pred,
+             xyz_target=xyz_target)
     return filename
