6e5475033e998db4589676b8078d912e72ed7d97
NettleL <nethya.liyanage@education.nsw.gov.au>
2025-06-10 16:21:11 +1000

    Added loading screen + ability to download final files

---
+++ SE_Project_Nethya_Liyanage/aimino/download.py
@@ -0,0 +1,17 @@
+import os
+import numpy as np
+from aimino.plot_prot import classical_mds
+
+def create_pdb_file(dist_map):
+    X = classical_mds(dist_map)
+    lines = []
+    for i, (x, y, z) in enumerate(X, start=1):
+        lines.append(f"ATOM  {i:5d}  CA  ALA A{i:4d}    {x:8.3f}{y:8.3f}{z:8.3f}  1.00  0.00           C")
+    lines.append("END")
+    return "\n".join(lines)
+
+def create_npz_file(dist_pred, dist_target, xyz_pred, xyz_target, key):
+    filename = f'{key}.npz'
+    file_path = os.path.join('static', 'uploads', filename)
+    np.savez(file_path, dist_pred=dist_pred, dist_target=dist_target, xyz_pred=xyz_pred, xyz_target=xyz_target)
+    return filename
